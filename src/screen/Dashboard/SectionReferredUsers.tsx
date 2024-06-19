18198dacfaf65552d8c8cf9d4981e623245fb686
tantaile421 <tantai.le421@gmail.com>
2024-06-20 00:22:54 +0700

    update all views background + done all

--- src/screen/Dashboard/SectionReferredUsers.tsx
+++ src/screen/Dashboard/SectionReferredUsers.tsx
@@ -1,6 +1,6 @@
 import { CustomCard } from "@component/DesignSystem/CustomCard"
 import { CustomTable } from "@component/DesignSystem/CustomTable"
-import { TableProps, Typography } from "antd"
+import { Flex, TableProps, Tag, Typography } from "antd"
 
 const { Text } = Typography
 
@@ -32,7 +32,13 @@ const data: DataType[] = []
 
 export const SectionReferredUsers = () => {
   return (
-    <CustomCard title="Your referred users">
+    <CustomCard title={
+      <Flex align="cent">
+        Your referred users
+        <Tag color="#E2F5FF" style={{ color: '#2578A6', marginLeft: 8 }}>0</Tag>
+      </Flex>
+
+    }>
       <CustomTable
         columns={columns}
       />
